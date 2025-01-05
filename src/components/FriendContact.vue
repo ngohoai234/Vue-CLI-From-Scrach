1a791b19ad7e1e821bdca9ef5a2a01fb0e5011e9
ngohoai234 <cangod05@gmail.com>
2025-01-05 21:10:42 +0700

    create FriendContact Component and use them into the app component

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong>{{ friend.phone }}</li>
+      <li><strong>Email: </strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "Can",
+        name: "Can Nguyen",
+        phone: "0933966978",
+        email: "cannv2@fpt.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
